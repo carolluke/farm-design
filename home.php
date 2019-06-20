1cfe59d6838e03cce30caeeff70ec7fae1a7b074
carolluke <gitongacarol11@gmail.com>
2019-06-20 15:59:01 +0300

    first commit

---
+++ home.php
@@ -0,0 +1,288 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>JumboTale flower farm</title>
+        <link rel="stylesheet" type="text/css" href="jumbotale.css">
+    </head>
+
+    <body>
+        <div class="container">
+
+        <header>
+
+            <div class="imgdisplay">
+                <img src="Jumbotale.png" alt="JumboTale Flower Farm">
+            </div>
+            
+        </header>
+        
+        <nav>
+            
+        
+            <ul class="tabs">
+                <li>
+                    <a href="#login" class="active">Login</a>
+                </li>
+                <li>
+                     <a href="#register">Register</a>
+                </li>
+              
+            </ul>
+            <div id="login" class="form-action show">
+                <h1>Login</h1>
+               
+            <form>
+                    
+                        
+                            <input type="text" placeholder="Username" />
+                        
+                        
+                            <input type="password" placeholder="Password" />
+                       
+                        
+                            <input type="submit" value="Login" class="button" />
+                        
+                    
+                </form>
+                <br/>
+                <hr>
+                <div>
+                    <aside>
+                
+                <h1>About JumboTale</h1>
+                <p>Jumbotale is the leading flower farm in naivasha county covering a total of 1003 acres.</p>
+
+                <p>Jumbotale flower farm was founde by Dr. Aston melvis in the year 1988 with an aim of improving the economic status of the area.</p>
+                <p>The farm has then grown marvellously with a number of awards both locally and internationally.</p>
+            </aside>
+            <article>                
+                <p>Jumbotale flower farm was founde by Dr. Aston melvis in the year 1988 with an aim of improving the economic status of the area.</p>
+                <p>The farm has then grown marvellously with a number of awards both locally and internationally.</p>
+            </article>
+            <article>                
+                <p>Jumbotale flower farm was founde by Dr. Aston melvis in the year 1988 with an aim of improving the economic status of the area.</p>
+                <p>The farm has then grown marvellously with a number of awards both locally and internationally.</p>
+            </article>
+            <article>                
+                <p>Jumbotale flower farm was founde by Dr. Aston melvis in the year 1988 with an aim of improving the economic status of the area.</p>
+                <p>The farm has then grown marvellously with a number of awards both locally and internationally.</p>
+            </article>
+            
+                </div>
+        </nav>
+        
+      
+    
+    
+        
+        
+        <section class="form">
+
+            <form action="" method="post" novalidate>
+                <fieldset>
+                    <div class="group1" style=" border: 2; border-width: 5px; border-style: dashed solid;" >
+                    <div class="item">
+                        <label>
+                            <span>Choose Greenhouse</span>
+                            <select class="required" name="dropdown">
+                                <option value="">-- none --</option>
+                                <option value="o1">Block 1</option>
+                                <option value="o2">Block 2</option>
+                                <option value="o3">Block 3</option>
+                                <option value="o3">Block 4</option>
+                                <option value="o3">Block 5</option>
+                            </select>
+                        </label>
+                        <div class='tooltip help'>
+                            <span>?</span>
+                            <div class='content'>
+                                <b></b>
+                                <p>Choose something or choose not. what shall it be?</p>
+                            </div>
+                        </div>
+                    </div>
+                     <div class="item">
+                        <label>
+                            <span>Choose Block Bed</span>
+                            <select class="required" name="dropdown">
+                                <option value="">-- none --</option>
+                                <option value="o1">Bed 1</option>
+                                <option value="o2">Bed 2</option>
+                                <option value="o3">Bed 3</option>
+                                
+                            </select>
+                        </label>
+                        <div class='tooltip help'>
+                            <span>?</span>
+                            <div class='content'>
+                                <b></b>
+                                <p>Choose something or choose not. what shall it be?</p>
+                            </div>
+                        </div>
+                    </div>
+
+                     <div class="item">
+                        <label>
+                            <span>Flower Type</span>
+                            <select class="required" name="dropdown">
+                                <option value="">-- none --</option>
+                                <option value="o1">Type 1</option>
+                                <option value="o2">Type  2</option>
+                                <option value="o3">Type 3</option>
+                                <option value="o3">Type 4</option>
+                                <option value="o3">Type 5</option>
+                            </select>
+                        </label>
+                        <div class='tooltip help'>
+                            <span>?</span>
+                            <div class='content'>
+                                <b></b>
+                                <p>Choose something or choose not. what shall it be?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <br/>
+                <div class="group" style="border: 2; border-width: 5px; border-style: dashed solid;">
+
+                     <div class="item">
+                        <label>
+                            <span>Week Entry</span>
+                            <select class="required" name="dropdown">
+                                <option value="">-- none --</option>
+                                <option value="o1">Entry 1</option>
+                                <option value="o2">Entry 2 2</option>
+                                <option value="o3">Entry 3</option>
+                                <option value="o3">Entry 4</option>
+                                <option value="o3">Entry 5</option>
+                            </select>
+                        </label>
+                        <div class='tooltip help'>
+                            <span>?</span>
+                            <div class='content'>
+                                <b></b>
+                                <p>Choose something or choose not. what shall it be?</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <label>
+                            <span>Date</span>
+                            <input class='date' type="date" name="date" required='required'>
+                        </label>
+                    </div>
+                    <div class="item">
+                        <label>
+                            <span>Section</span>
+                            <select class="required" name="dropdown">
+                                <option value="">-- none --</option>
+                                <option value="o1">Top</option>
+                                <option value="o2">Middle</option>
+                                <option value="o3">Base</option>
+                                
+                            </select>
+                        </label>
+                        <div class='tooltip help'>
+                            <span>?</span>
+                            <div class='content'>
+                                <b></b>
+                                <p>Choose something or choose not. what shall it be?</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <br/>
+                <br/>
+                <br/>
+
+                   
+                    
+                    
+                    
+                           
+                    
+                        <fieldset style="clear:both; margin: 0; 
+                padding: 0; border-width: 5px; border-style: dashed solid;">
+                            <legend style="font: bold;">Health</legend>
+                            <br/>
+                            <br/>
+                            <div class="item">
+                        <label>
+                            <span>Water</span>
+                            <textarea required="required" name='message'></textarea>
+                        </label>
+                    </div>
+                    <br/>
+                            
+                    <div class="item">
+                        <label>
+                            <span>Nutrition</span>
+                            <textarea required="required" name='message'></textarea>
+                        </label>
+                    </div>
+                    <br/>
+                            <br/>
+                </fieldset>
+                <br/>
+                <br/>
+                <fieldset style="clear:both; margin: 0; 
+                padding: 0; border-width: 5px; border-style: dashed solid;" > <legend> Bugs</legend>
+                
+                <div class="item">
+                    <label>Bugs</label>
+                    <input type="checkbox" name="null" value="bugs">Null<br>      
+        <input type="checkbox" name="present" value="bugs">Present<br> 
+                 </div>
+                 <p>If Present enter:</p>
+
+                     <div class="item">
+
+                        <label>
+                            <span>Name</span>
+
+                            <input type="text" name="name" placeholder="name" required="required"  />      
+                        </label>
+                       
+                    </div>
+                    <div class="item">
+                        <label>
+                            <span>Bug Stage</span>
+                            <select class="required" name="dropdown">
+                                <option value="">-- none --</option>
+                                <option value="o1">larvae</option>
+                                <option value="o2">adult</option>
+                                
+                                
+                            </select>
+                        </label>
+                        <div class='tooltip help'>
+                            <span>?</span>
+                            <div class='content'>
+                                <b></b>
+                                <p>Choose something or choose not. what shall it be?</p>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    </fieldset>
+                    <br/>
+                    
+
+                       <button id='send' type='submit'>Submit</button>
+             
+                    </fieldset>
+                </form>
+                     </section>
+
+                  
+    
+       </div>
+
+           <footer>
+            <p>@copyright: jumbotale.co.ke</p>
+        </footer>
+    </body>
+</html>
